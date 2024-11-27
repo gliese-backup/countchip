3219ea12a7190240c6ee4d92f437a436ffe8e979
gliese-backup <glieseb+1@proton.me>
2024-11-27 13:57:48 +0000

    Learn useState

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,24 @@
+import { useState } from "react";
+
 const Counter = () => {
+  let [counter, updateCounter] = useState(40);
+
+  // let stateValue = stateSeAayiHuiValue[0];
+  // const updateKarneWalaFunction = stateSeAayiHuiValue[1];
+
+  function handleClick() {
+    updateCounter(counter + 1);
+  }
+
   return (
     <div className="flex items-center justify-center h-20 w-72 border-2 border-black rounded">
-      <button className="bg-blue-500 text-white p-2 rounded-md">
+      <button
+        className="bg-blue-500 text-white p-2 rounded-md"
+        onClick={handleClick}
+      >
         Increment
       </button>
-      <p>Count: 0</p>
+      <p>Count: {counter}</p>
       <button className="bg-blue-500 text-white p-2 rounded-md">
         Decrement
       </button>
